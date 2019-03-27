b2cd48abd13e62191161126b53861538c46c7d55
MHK <mhk@Majids-MacBook-Pro.local>
2019-03-27 21:06:03 +0000

    fixed register file path

--- pages/register.php
+++ pages/register.php
@@ -1,22 +1,18 @@
 <?php
 
-require_once dirname(__DIR__) . "/pages/views.php";
-
-
+require_once dirname(__DIR__).'/pages/views.php';
 
 if (isset($_POST['action'])) {
-    
     $user = new User();
     if ($_POST['action'] === 'REGISTER') {
         $user->RegisterUser($_POST);
         $type = Session::flashType();
         if ($type == 'danger' || $type == 'warning') {
-            echo $twig->render('register.twig', array('page' => 'New Membership', 'message' => Session::flash(), 'type' => $type, 'title' => 'Whoops..!'));
+            echo $twig->render('mp-client/register.twig', array('page' => 'New Membership', 'message' => Session::flash(), 'type' => $type, 'title' => 'Whoops..!'));
         } else {
-           echo $twig->render('login.twig', array('page' => 'Members Login', 'message' => Session::flash(), 'type' => $type, 'title' => 'Whoops..!'));
+            echo $twig->render('mp-client/login.twig', array('page' => 'Members Login', 'message' => Session::flash(), 'type' => $type, 'title' => 'Whoops..!'));
         }
     }
-
 } else {
-    echo $twig->render('register.twig', array('page' => 'New Membership'));
+    echo $twig->render('mp-client/register.twig', array('page' => 'New Membership'));
 }
