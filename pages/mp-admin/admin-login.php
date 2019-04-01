f3b60bc245eb8ce182767a1c2633ff102f77904a
muhammad <muhammad@g3t.co.uk>
2019-04-01 16:27:02 +0100

    some changes

--- pages/mp-admin/admin-login.php
+++ pages/mp-admin/admin-login.php
@@ -3,6 +3,12 @@
 require_once dirname(__DIR__).'../../pages/views.php';
 $user = new User();
 
+$stock = new Stock();
+$outof_stock = $stock->checkOutOfStock();
+$low_stock = $stock->checkLowStock();
+$outof_stock = $stock->checkOutOfStock();
+$outof_stock = $stock->checkOutOfStock();
+
 if (isset($_POST['action'])) {
     if ($_POST['action'] === 'LOGIN') {
         $user->AdminLogin($_POST);
@@ -12,11 +18,20 @@ if (isset($_POST['action'])) {
         } else {
             $product = new Products();
             echo $twig->render('mp-admin/admin-dashboard.twig', array('products' => $product->getAllMeals(),
-                'page' => 'My Account',
+                'page' => 'Dashboard',
+                'subtitle' => 'Control panel',
                 'message' => Session::flash(),
                 'type' => Session::flashType(),
                 'title' => 'Success..!', 'path' => IMAGES,
-                'fullname' => Session::get('fullname'), ));
+                'fullname' => Session::get('fullname'),
+              
+                'outofstock' => sizeof($outof_stock),
+                'lowstock' => sizeof($low_stock),
+                'today' => 20,
+                'nextweek' => 30, 'path' => IMAGES,
+                'fullname' => Session::get('fullname'),
+            
+            ));
         }
     }
 } else {
