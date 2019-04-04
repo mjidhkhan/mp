3ccef2b332a31bd2372db92b33eebe5559571239
Majid Hameed Khan <majidhkhan@outlook.com>
2019-04-04 22:34:54 +0100

    query change

--- classes/Stock.php
+++ classes/Stock.php
@@ -124,7 +124,7 @@ class Stock extends Model
 
     public function getOutOfStock(){
 
-        $this->sql = $this->db->prepare('SELECT * FROM ingredients WHERE quantity < reorder_level');
+        $this->sql = $this->db->prepare('SELECT * FROM ingredients WHERE quantity between 0 AND  reorder_level');
         $this->sql->execute();
         $count = $this->sql->rowCount();
         if ($count > 0) {
