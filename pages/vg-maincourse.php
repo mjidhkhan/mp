72966bbb5370726d8dbfce10f522fa6938d7efa9
muhammad <muhammad@g3t.co.uk>
2019-04-05 16:23:50 +0100

    veg courses

--- pages/vg-maincourse.php
+++ pages/vg-maincourse.php
@@ -1,6 +1,11 @@
 <?php
+
 require_once dirname(__DIR__).'/pages/views.php';
 
 $product = new Products();
-echo $twig->render('vg-maincourses.twig', array('products' => $product->getAllNonVegetarian(ID),'page' => 'Desserts', 'login'=>'Login','path' => IMAGES));
-
+if(Session::get('ststus')== 3){
+    $login = 'Logout';
+}else{
+    $login = 'Login';
+}
+echo $twig->render('vg-maincourses.twig', array('products' => $product->getAllNonVegetarianMains(),'page' => 'Vegetarian Main Courses','login'=>$login, 'path' => IMAGES));
