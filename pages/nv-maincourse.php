bc1a72036a50bc45c50d118d5077eb0384026a8e
muhammad <muhammad@g3t.co.uk>
2019-04-05 10:56:16 +0100

    working with front end

---
+++ pages/nv-maincourse.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(__DIR__).'/pages/views.php';
+
+$product = new Products();
+echo $twig->render('nv-maincourses.twig', array('products' => $product->getAllNonVegetarian(ID),'page' => 'Desserts', 'path' => IMAGES));
+
