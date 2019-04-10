da4d253478eb97c7469b765dc66d64130c177fe5
muhammad <muhammad@g3t.co.uk>
2019-04-10 12:37:54 +0100

    load single product

--- pages/mp-admin/outof_stock.php
+++ pages/mp-admin/outof_stock.php
@@ -2,19 +2,17 @@
 require_once dirname(__DIR__).'../../pages/views.php';
 
 
-	if (Session::get('status') == 1 || Session::get('status') == 2 ) {
+if (Session::get('status') == 1 || Session::get('status') == 2 ) {
 
-         
-	
-
-echo $twig->render('mp-admin/outofstock-items.twig', array('stock' => 'Stock', 
+	echo $twig->render('mp-admin/outofstock-items.twig', array('stock' => 'Stock', 
 								'page' => 'Out of Stock', 
-								'outofstock' => sizeof($outof_stock),
-								'lowstock' => sizeof($low_stock),
+								'outofstock' => $outof_stock,
+								'lowstock' => $low_stock,
 								'today' => 20,
 								'nextweek' => 30, 
 								'path' => IMAGES,
-								'stock'=>$data,'admin_data' =>$details, 
+								'stock'=>$data,
+								'admin_data' =>$details, 
 								'stock_data'=>$stock_data,
 								'order_data'=>$order_data));
 	}
